7c97c13af8108dfffce190deab4af433017c7480
Ishini-0313 <misnfernando1@gmail.com>
2026-07-16 10:31:20 +0530

    application_amendment table created

---
+++ app/Models/Application_amendments.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Application;
+use App\Models\Workflow_steps;
+use App\Models\Amendment_workflow_histories;
+
+class Application_amendments extends Model
+{
+    protected $fillable = [
+        'application_id',
+        'new_leave_start_date',
+        'new_leave_end_date',
+        'reason_for_change',
+        'workflow_id',
+        'current_step_id',
+        'current_assigned_user_id',
+        'current_assigned_office_id',
+        'status',
+        'approved_at'
+    ];
+
+    public function application(){
+        return $this->belongsTo(Application::class);
+    }
+
+    public function current_step(){
+        return $this->belongsTo(Workflow_steps::class, 'current_step_id');
+    }
+
+    public function workflowHistories(){
+        return $this->hasMany(Amendment_workflow_histories::class);
+    }
+}
