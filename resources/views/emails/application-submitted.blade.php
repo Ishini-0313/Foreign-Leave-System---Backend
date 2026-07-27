f0df991cc05e4c4ffdf0553aac262b367d64fcbd
Ishini-0313 <misnfernando1@gmail.com>
2026-07-27 10:19:41 +0530

    application submission mail template created

---
+++ resources/views/emails/application-submitted.blade.php
@@ -0,0 +1,16 @@
+<h2>Application Submitted Successfully</h2>
+
+<p>Dear {{ $application->name }},</p>
+
+<p>Your foreign leave application has been submitted successfully.</p>
+
+<p>
+Application No:
+<strong>{{ $application->application_no }}</strong>
+</p>
+
+<p>
+You can log into the system to track your application.
+</p>
+
+<p>Thank you.</p>
